4e666c24e09a1d8e441a5f0a4b1ad3eb5ac9c22a
Joshua Puello <joshuapuello@outlook.com>
2019-06-29 11:30:18 -0500

    [JPP] - Adding Selection sort

---
+++ src/com/company/datastructures/SelectionSort.java
@@ -0,0 +1,42 @@
+package com.company.datastructures;
+
+/**
+ * - in-place algorithm
+ * - O(n^2) time complexity - quadratic
+ * - It will take 100 steps to sort 10 items
+ * - Doesn't require as much swapping as bubble sort
+ * - Unstable algorithm
+ */
+public class SelectionSort {
+
+    public static void main(String[] args) {
+
+        int[] array = { 20, 18, -15, 1, 36, 57, -22 };
+
+        for (int lastUnsortedIndex = array.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+            int largestIndex = 0;
+            for (int i = 0; i <= lastUnsortedIndex; i++) {
+                if (array[i] > array[largestIndex]) {
+                    largestIndex = i;
+                }
+            }
+            swap(array, largestIndex, lastUnsortedIndex);
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+
+    }
+
+    private static void swap(int[] array, int i, int j) {
+
+        if (i == j) return;
+
+        int tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
+
+    }
+
+}
