156c974d9d202ea34f1ace1fbf6ed7419ad8906e
Joshua Puello <joshuapuello@outlook.com>
2019-07-05 17:52:56 -0500

    [JPP] - Unicode elements

--- src/main/java/com/company/utils/Utils.java
+++ src/main/java/com/company/utils/Utils.java
@@ -5,7 +5,7 @@ public final class Utils {
     static String stringFormat = "%s has %s";
 
     public static void printAssertEquals(String testName, String s1, String s2) {
-        final String result = String.format(stringFormat, testName, (s1.equals(s2) ? "passed" : "failed"));
+        final String result = String.format(stringFormat, testName, (s1.equals(s2) ? "passed \u2611" : "failed \u2612"));
         System.out.println("Utils.printAssertEquals | " + result);
     }
 
