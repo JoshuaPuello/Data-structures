0691163b6cb40548f8ea392cdf237f51ccfd47ef
Joshua Puello <joshuapuello@outlook.com>
2019-07-05 18:40:10 -0500

    [JPP] - Added method using sort algorithm

--- src/main/java/com/company/exercises/arrays/TwoNumbersSum.java
+++ src/main/java/com/company/exercises/arrays/TwoNumbersSum.java
@@ -27,7 +27,7 @@ public class TwoNumbersSum {
 
         for (Object[] input : getInputs()) {
             Utils.printAssertEquals(String.valueOf(input[TEST_NAME_INDEX]),
-                    Arrays.toString(twoNumbersSum((int[]) input[ARRAY_INDEX], (int) input[SUM_INDEX])),
+                    Arrays.toString(twoNumbersSumSorting((int[]) input[ARRAY_INDEX], (int) input[SUM_INDEX])),
                     Arrays.toString((int[]) input[EXPECTED_INDEX]));
         }
 
@@ -45,8 +45,9 @@ public class TwoNumbersSum {
      * Since stream is lazy loading, time complexity for filter is O(n) in the worst case
      * We can use Map instead of /int[] complement/
      * Search operation in Map is 0(1) in most of the cases
+     * Space complexity O(n)
      */
-    private static int[] twoNumbersSum(int[] array, int sum) {
+    private static int[] twoNumbersSumComplement(int[] array, int sum) {
 
         // HashMap<Integer, Integer> complement = new HashMap<>();
         int[] complement = new int[array.length];
@@ -62,6 +63,27 @@ public class TwoNumbersSum {
             }
             // complement.put(complementInt, complementInt)
             complement[i] = complementInt;
+        }
+
+        return new int[] {};
+    }
+
+    /**
+     * Since Arrays.sort TC is O(n log (n))
+     * Space complexity is O(1)
+     */
+    private static int[] twoNumbersSumSorting(int[] array, int sum) {
+
+        Arrays.sort(array);
+
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left < right) {
+            int sumBothSides = array[left] + array[right];
+            if (sumBothSides > sum) right--;
+            else if (sumBothSides < sum) left++;
+            else return new int[] { array[left], array[right] };
         }
 
         return new int[] {};
