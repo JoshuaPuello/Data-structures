0afd1dea4697a3021c0c111cb8cdcb00ccd6e41f
Joshua Puello <joshuapuello@outlook.com>
2019-07-06 18:01:05 -0500

    [JPP] - Added solving methods

--- src/main/java/com/company/exercises/searching/ThreeLargestNumbers.java
+++ src/main/java/com/company/exercises/searching/ThreeLargestNumbers.java
@@ -1,5 +1,10 @@
 package com.company.exercises.searching;
 
+import com.company.utils.Utils;
+
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Write a function that takes in an array of integers and returns a sorted array of the three largest
  * integers in the input array. Note that the function should return duplicate integers if necessary;
@@ -9,5 +14,60 @@ package com.company.exercises.searching;
  * Sample output: [18, 141, 541]
  */
 public class ThreeLargestNumbers {
+
+    public static final int EXPECTED_INDEX = 2;
+    public static final int INPUT_INDEX = 1;
+    public static final int TEST_NAME_INDEX = 0;
+
+    public static void main(String[] args) {
+
+        for (Object[] input : getInputs()) {
+            Utils.printAssertEquals(String.valueOf(input[TEST_NAME_INDEX]),
+                    Arrays.toString(updateLargestNumbers((int[]) input[INPUT_INDEX])),
+                    Arrays.toString((int[]) input[EXPECTED_INDEX]));
+        }
+
+    }
+
+    /**
+     * Time complexity 0(n)
+     * Space complexity O(1)
+     */
+    private static List<Object[]> getInputs() {
+        Object[] test1 = new Object[] { "Test 1", new int[] { 55, 7, 8 }, new int[] { 7, 8, 55 } };
+        Object[] test2 = new Object[] { "Test 2", new int[] { 55, 43, 11, 3, -3, 10 }, new int[] { 11, 43, 55 } };
+        Object[] test3 = new Object[] { "Test 3", new int[] { 7, 8, 3, 11, 43, 55 }, new int[] { 11, 43, 55 } };
+        Object[] test4 = new Object[] { "Test 4", new int[] { 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7 }, new int[] { 7, 7, 7 } };
+        Object[] test5 = new Object[] { "Test 5", new int[] { -1, -2, -3, -7, -17, -27, -18, -541, -8, -7, 7 }, new int[] { -2, -1, 7 } };
+        return Arrays.asList(test1, test2, test3, test4, test5);
+    }
+
+    private static int[] updateLargestNumbers(int[] numbers) {
+        int[] largestNums = { Integer.MIN_VALUE, Integer.MIN_VALUE, Integer.MIN_VALUE };
+
+        for (int number : numbers) {
+            if (number > largestNums[2]) insertLargestNum(largestNums, number, 2);
+            else if (number > largestNums[1]) insertLargestNum(largestNums, number, 1);
+            else if (number > largestNums[0]) insertLargestNum(largestNums, number, 0);
+        }
+
+        return largestNums;
+    }
+
+    private static void insertLargestNum(int[] largestNums, int number, int index) {
+        int current = largestNums[index];
+        largestNums[index] = number;
+        switch (index) {
+            case 2: {
+                largestNums[index - 2] = largestNums[index - 1];
+                largestNums[index - 1] = current;
+                break;
+            }
+            case 1: {
+                largestNums[index - 1] = current;
+                break;
+            }
+        }
+    }
 
 }
