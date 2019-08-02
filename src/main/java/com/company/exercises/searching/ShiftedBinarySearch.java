d75667218951413f5e19c027b6d460a891e26ba7
Joshua Puello <joshuapuello@outlook.com>
2019-08-01 22:32:32 -0500

    [JPP] - Shifted Binary Search alternative methods

--- src/main/java/com/company/exercises/searching/ShiftedBinarySearch.java
+++ src/main/java/com/company/exercises/searching/ShiftedBinarySearch.java
@@ -41,14 +41,14 @@ public class ShiftedBinarySearch {
         return tests;
     }
 
-    /**
-     * Time complexity: O(log(n))
-     * Space complexity: O(log(n))
-     */
     private static int shiftedBinarySearch(int[] array, int target) {
         return shiftedBinarySearch(array, target, 0, array.length - 1);
     }
 
+    /**
+     * Time complexity: O(log(n))
+     * Space complexity: O(log(n))
+     */
     private static int shiftedBinarySearch(int[] array, int target, int left, int right) {
 
         if (left > right) {
@@ -76,6 +76,39 @@ public class ShiftedBinarySearch {
             }
         }
 
+    }
+
+    /**
+     * Time complexity: O(log(n))
+     * Space complexity: O(1)
+     */
+    public static int shiftedBinarySearchPointers(int[] array, int target, int left, int right) {
+
+        while (left <= right) {
+
+            int middle = (left + right) / 2;
+            int potentialMatch = array[middle];
+            int leftNum = array[left];
+            int rightNum = array[right];
+
+            if (target == potentialMatch) {
+                return middle;
+            } else if (leftNum <= potentialMatch) {
+                if (target < potentialMatch && target >= leftNum) {
+                    right = middle - 1;
+                } else {
+                    left = middle + 1;
+                }
+            } else {
+                if (target > potentialMatch && target <= rightNum) {
+                    left = middle + 1;
+                } else {
+                    right = middle - 1;
+                }
+            }
+        }
+
+        return -1;
     }
 
 }
